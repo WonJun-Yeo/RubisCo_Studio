aa97b2d7f242cb5d6bee3cfa4f7dd5721053a230
WonJun Yeo <rbgh004@gmail.com>
2022-03-26 20:03:42 +0900

    [create] Implementation of header arrow buttons on reactive web

---
+++ JS/headerArrow.js
@@ -0,0 +1,14 @@
+var arrow = 1;
+
+
+$('.header-arrow').on('click', function() {
+    if (arrow == 1) {
+        $('.header-list').css('transform', 'translateX(0)');
+        $('.header-arrow').css('transform', 'rotate(180deg)');
+        arrow = 2;
+    } else if (arrow == 2) {
+        $('.header-list').css('transform', 'translateX(90%)');
+        $('.header-arrow').css('transform', 'rotate(0)');
+        arrow = 1;
+    }
+})
